bdbc8e92725cdb4b8a23784f1af5c5923c1532e8
lakshmilanguage <laxmijayaamara@gmail.com>
2026-06-15 22:29:37 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,37 @@
+while True:
+
+    print("\n----- Calculator -----")
+
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = input("Enter Choice: ")
+
+    if choice == "5":
+        print("Thank You")
+        break
+
+    num1 = float(input("Enter First Number: "))
+    num2 = float(input("Enter Second Number: "))
+
+    if choice == "1":
+        print("Result =", num1 + num2)
+
+    elif choice == "2":
+        print("Result =", num1 - num2)
+
+    elif choice == "3":
+        print("Result =", num1 * num2)
+
+    elif choice == "4":
+
+        if num2 == 0:
+            print("Cannot Divide By Zero")
+        else:
+            print("Result =", num1 / num2)
+
+    else:
+        print("Invalid Choice")
